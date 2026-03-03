db9c056212d75b23bb22fe94dcb9612cec321767
the2nake <41810559+the2nake@users.noreply.github.com>
2026-03-03 08:04:22 -0500

    initial commit: add basic read and plot scripts

---
+++ serial_monitor.py
@@ -0,0 +1,23 @@
+import signal
+import serial
+import sys
+
+
+def interrupt_handler(signal, frame):
+    print(" [interrupt] ")
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, interrupt_handler)
+
+ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
+print("using port", ser.name, "@", ser.baudrate)
+
+while True:
+    data = ser.readline().decode().strip()
+    if not data:
+        continue
+    print("[rec]", data)
+
+    if len(data) > 3 and data[:3] == "IMU":
+        print("imu data:", data[3:])
