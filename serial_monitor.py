32618f9a07d640b5a06f6243957981b2c4d18f62
the2nake <41810559+the2nake@users.noreply.github.com>
2026-03-09 14:28:01 -0400

    feat: buffer longer lines

--- serial_monitor.py
+++ serial_monitor.py
@@ -10,12 +10,18 @@ def interrupt_handler(signal, frame):
 
 signal.signal(signal.SIGINT, interrupt_handler)
 
+sys.stdout = open(1, "w", buffering=100000)
+
 ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
 print("using port", ser.name, "@", ser.baudrate)
+
 
 while True:
     data = ser.readline().decode().strip()
     if not data:
         continue
+
     print("[rec]", data)
-    
+
+    if data.startswith("UPPER"):
+        sys.stdout.flush()
